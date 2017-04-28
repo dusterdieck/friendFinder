88f0d118b842f2ff1971a57413d81f602812bac1
Timothy Dusterdieck <timothy@masseywholesale.com>
2017-04-28 19:13:18 -0400

    Server file, routing, and data file added.

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,58 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+var friendData = require("../data/friends");
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+module.exports = function(app) {
+  // API GET Requests
+  // ---------------------------------------------------------------------------
+  app.get("/api/friends", function(req, res) {
+    res.json(friendData);
+  });
+
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // returns best matching friend
+  // ---------------------------------------------------------------------------
+  app.post("/api/friends", function(req, res) {
+      //more useful name for posted data
+      let user = req.body;
+      //console.log(user);
+
+      let minScore = 41; //1 more than maximum possible aggregate difference. should probably be dynamic, in the case more survey questions were added, but meh
+      let mostCompatible; //variable for storing index of most compatible
+      //loops through each user in friendData
+      for(let i = 0; i < friendData.length; i++){
+        //sets the array to be compared to the scores array in the current user
+        var currentCompare = friendData[i].scores;
+        //zeroes out the current comparison score
+        var currScore = 0;
+        //loops through the scores array of both the posted user and the current user from the stored data
+        for(let j = 0; j < 10; j++){
+           //sums the absolute value of the differences of each score
+           currScore += Math.abs( user.scores[j].parseInt() - currentCompare[j].parseInt() );
+        }
+        //after loop is done, if the current score is less than the minimum score, sets that index to the most compatible
+        if (currScore < minScore) {
+          minScore = currScore;
+          mostCompatible = i;
+        }
+      }
+
+      //push new user to our friend data. important to do this after the compatibility check, otherwise it would always return the user as most compatible with themselves.
+      friendData.push(user);
+      //after it's checked all of the users, returns the full object for the most compatible user in the data
+      return friendData[ mostCompatible ];
+
+
+  });
+  // clear data, basically just for testing purposes
+
+  app.post("/api/clear", function() {
+    // Empty out the arrays of data
+    friendData = [];
+    console.log(friendData);
+  });
+};
---
+++ server.js
@@ -0,0 +1,27 @@
+const express=require('express'),
+	  bodyParser=require('body-parser'),
+	  path=require('path');
+
+const app = express(),
+	  PORT = 8080;
+
+
+// Body Parser stuff
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// including static dir stuff
+app.use(express.static(path.join(__dirname, 'app')));
+
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+
+app.listen(PORT);
